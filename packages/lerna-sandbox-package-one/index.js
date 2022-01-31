94cd4354974f862f3f298f981b28c389bd22490e
Quinn Lee <leekeunhwi@gmail.com>
2022-01-31 00:02:13 -0500

    feat: bar

--- packages/lerna-sandbox-package-one/index.js
+++ packages/lerna-sandbox-package-one/index.js
@@ -2,4 +2,11 @@ function packageOne() {
     return "I am an awesome package (1)";
 }
 
+function packageOne() {
+    return "I am an awesome package (1)";
+}
+
+function packageOne() {
+    return "I am an awesome package (1)";
+}
 exports.packageOne = packageOne;
